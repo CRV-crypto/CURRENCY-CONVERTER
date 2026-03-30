55170b205b7d34dba018874a55f6f52bda241e1f
CRV-crypto <rahulgootam7@gmail.com>
2026-03-30 21:15:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+let fromCurrency = document.getElementById("fromCurrency");
+let toCurrency = document.getElementById("toCurrency");
+
+// Load ALL currencies
+async function loadCurrencies() {
+    let res = await fetch("https://open.er-api.com/v6/latest/USD");
+    let data = await res.json();
+
+    let currencies = Object.keys(data.rates);
+
+    currencies.forEach(currency => {
+        fromCurrency.add(new Option(currency, currency));
+        toCurrency.add(new Option(currency, currency));
+    });
+
+    fromCurrency.value = "USD";
+    toCurrency.value = "INR";
+}
+
+loadCurrencies();
+
+// Convert
+async function convert() {
+    let amount = document.getElementById("amount").value;
+    let from = fromCurrency.value;
+    let to = toCurrency.value;
+
+    if (!amount) {
+        alert("Enter amount");
+        return;
+    }
+
+    document.getElementById("result").innerText = "Converting... ⏳";
+
+    let res = await fetch(`https://open.er-api.com/v6/latest/${from}`);
+    let data = await res.json();
+
+    let rate = data.rates[to];
+    let result = amount * rate;
+
+    document.getElementById("result").innerText =
+        `💰 ${amount} ${from} = ${result.toFixed(2)} ${to}`;
+}
+
+// Swap
+function swap() {
+    let temp = fromCurrency.value;
+    fromCurrency.value = toCurrency.value;
+    toCurrency.value = temp;
+}
+
+// Dark Mode
+function toggleMode() {
+    document.body.classList.toggle("dark");
+}
